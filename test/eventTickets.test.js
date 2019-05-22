82b3c83dfadf20c163b47750dd139a45d630a2f8
ConsenSys Academy <41963038+ConsenSys-Academy@users.noreply.github.com>
2019-05-22 17:44:23 -0400

    Update eventTickets.test.js

--- test/eventTickets.test.js
+++ test/eventTickets.test.js
@@ -1,3 +1,5 @@
+// The public file for automated testing can be found here: https://gist.github.com/ConsenSys-Academy/e9ec0d8d6c53b56ca9673cfa139b5644
+
 var EventTickets = artifacts.require('EventTickets')
 let catchRevert = require("./exceptionsHelpers.js").catchRevert
 
